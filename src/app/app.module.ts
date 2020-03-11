6ab03331d22d287496938f63eee962e238f02234
Chantal Neo <Chantal@hotmail.sg>
2020-03-12 00:44:22 +0800

    Understanding Navigation Paths, close #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { ServersService } from './servers/servers.service';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },         // localhost:4200
   { path: 'users', component: UsersComponent },   // localhost:4200/users
-  { path: 'servers', component: ServerComponent } // localhost:4200/servers
+  { path: 'servers', component: ServersComponent } // localhost:4200/servers
 ];
 
 @NgModule({
