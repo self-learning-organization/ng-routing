18abff699b76224d355598ca8e55ac7ad4df13c8
Chantal Neo <Chantal@hotmail.sg>
2020-03-12 21:07:09 +0800

    Understanding Location Strategies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,6 +40,7 @@ const appRoutes: Routes = [
 
 @NgModule({
     imports: [
+        // RouterModule.forRoot(appRoutes, {useHash: true}) 
         RouterModule.forRoot(appRoutes)
     ],
     exports: [
